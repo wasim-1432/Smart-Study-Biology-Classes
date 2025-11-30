8142b1eff97f67dc888c1051971ec11a2ac5152d
Mohd Wasim <wasim.ahmad0661@gmail.com>
2025-11-30 12:41:25 +0530

    Update ALLOWED_HOSTS to allow all hosts

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l1&zseid6fwzqvsr_+vlfsqkh$pe-3=412cvr4ixjs6=ixe()3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
